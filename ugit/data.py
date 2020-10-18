42f6a09548968081e2c2cef23ca1eb6241240e4a
hy.lee <hylee@madup.com>
2020-10-18 23:10:29 +0900

    add objects dir

--- ugit/data.py
+++ ugit/data.py
@@ -1,8 +1,18 @@
-import os
+import hashlib
+from pathlib import Path
 
 
-GIT_DIR = ".ugit"
+GIT_DIR = Path(".ugit")
+GIT_OBJECTS_DIR = GIT_DIR / Path("objects")
 
 
 def init():
-    os.makedirs(GIT_DIR)
+    GIT_DIR.mkdir(exist_ok=True)
+    GIT_OBJECTS_DIR.mkdir()
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f"{GIT_OBJECTS_DIR}/{oid}", "wb") as out:
+        out.write(data)
+    return oid
